daf2df04099ddfff4c5bab8b292bee438423efeb
zhuojinliang <zhuojinliang@aukeys.com>
2019-10-15 17:58:17 +0800

    remove unused files

--- test/smoke/template/dist/3_afc4de1f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{7:function(n,e,t){"use strict";t.r(e);var o=t(0),r=t.n(o);e.default=function(){return r.a.createElement("div",null,"测试 动态Import")}}}]);
