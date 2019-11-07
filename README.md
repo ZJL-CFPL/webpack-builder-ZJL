95db1c550b5487f6a16322dc1f2d42d36f9a9372
zhuojinliang <zhuojinliang@aukeys.com>
2019-11-07 17:09:30 +0800

    更新 文档介绍，删除容易

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# webpack-builder-ZJL
-webpack first try
+# webpack-builder-ZJL 描述
+基础版本前端工作流，主要用于多页面静态网站搭建。比如数据交互少的展示性官网开发、H5页面开发。
+后期将会搭建Vue ,React工作流。
+敬请期待~
+## Installation
+$ npm install
+### start
+$ npm run dev
+### build
+$ npm run build
+## Features
+* 工作目录: src
+* 打包输出目录： dist
+* 静态资源目录：src/assets
+* 页面编辑目录：src/pages
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,37 @@
 import './index.scss';
+import $ from 'jquery';
+
+var ajaxPost = function (type,data,success,error) {
+  $.ajax({
+    url: 'http://10.3.23.180/api/website/koronaNewsletter/addKoronaNewsletter',
+    type: type || 'post',
+    data: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    dataType: 'json',
+    success: success,
+    error: error
+  });
+};
+
+$('#sign1').on('click',function(){
+  var email1Todo =  new Check('#email1');
+  // console.log(email1Todo.isEmail());
+  if(email1Todo.isEmail()){
+    ajaxPost('post',{email:email1Todo.EleValue},function(res){
+      console.log(res);
+      if(res.ret == 1){
+        new Popup('Congratulations!</br> You have subscribed successfully.')
+      }
+    },function(err){alert(err)})
+  }
+})
+$('#btn').on('click',function(){
+  ajaxPost('post',{email:'test22@qq.com'},function(res){
+    console.log(res);
+    if(res.ret == 1){
+      console.log('Congratulations!</br> You have subscribed successfully.')
+    }
+  },function(err){alert(err)})
+})
